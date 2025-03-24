3a965f98ca62be50da4b966fdda62f072b2e5adf
Royalroman123 <136818296+Royalroman123@users.noreply.github.com>
2025-03-24 11:28:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,10 +89,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/62c76ac2095332a0ede75.jpg"
+    "https://unitedcamps.in/Images/IMG_1742795844.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
+    "PING_IMG_URL", "https://unitedcamps.in/Images/IMG_1742795844.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4310ea5f523520b2b765b.jpg"
